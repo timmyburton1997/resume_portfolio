0f61f9c5eb7ead3905b453bab0badab69b24ab7d
Tim <timmy.burton1997@gmail.com>
2019-08-10 13:22:31 -0400

    Adding secondary apps

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Jobs(models.Model):
+    image = models.ImageField(upload_to='')
+    summary = models.CharField(max_length=255)
+    title = models.TextField()
+    time = models.DateTimeField()
